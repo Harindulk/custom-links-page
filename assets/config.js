6bee33e06dd1f59eaa8d4cf938b90dc2d39e840a
Aliya khan <143610535+ELINA-BOTS@users.noreply.github.com>
2023-11-16 20:15:40 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -4,44 +4,24 @@ let config = {
   "Description": "Your Description",
   "links": [
     {
-      "Title": "Website",
-      "URL": "http://www.harindu.dev",
-      "icon_classes": "fas fa-link",
+      "Title": "telegram",
+      "URL": "https://t.me/IM_JISSHU",
+      "icon_classes": "fab fa-telegram",
+    },
+    {
+      "Title": "YouTube",
+      "URL": "https://youtube.com/@JISSHU-BOTS?si=jKijmi9isE_YSHpX",
+      "icon_classes": "fab fa-youtube",
+    },
+    {
+      "Title": "TELEGRAM CHANNEL",
+      "URL": "https://t.me/JISSHU_BOTS",
+      "icon_classes": "fab fa-telegram"
     },
     {
       "Title": "Github",
-      "URL": "https://github.com/harindulk",
+      "URL": "https://github.com/Jisshubot",
       "icon_classes": "fab fa-github",
-    },
-    {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
-      "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
-    },
-    {
-      "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
     },
   ]
 }
